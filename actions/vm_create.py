b27e13e04288dd96f233d9f3cb8b3a9b7c8f8bbe
Mukesh Kumar_5375 <mukesh.kumar_5375@nihilent.com>
2020-01-09 02:04:57 +0530

    added Windows Virtual Machine

--- actions/vm_create.py
+++ actions/vm_create.py
@@ -18,7 +18,7 @@ haikunator = Haikunator()
 LOCATION = 'Southeast Asia'
 
 # Resource Group
-GROUP_NAME = 'azure-sample-group-virtual-machines5'
+GROUP_NAME = 'azure-group'
 
 # Network
 VNET_NAME = 'azure-vnet'
@@ -32,7 +32,8 @@ IP_CONFIG_NAME = '203.18.137.2'
 NIC_NAME = 'azure-nic'
 USERNAME = 'mukesh.kumar_5375@nihilent.com'
 PASSWORD = 'nihilent@123'
-VM_NAME = 'New-VM'
+VM_NAME = 'Linux-VM'
+W_VM_NAME = 'Windows-VM'
 
 VM_REFERENCE = {
     'linux': {
@@ -102,23 +103,8 @@ class VmCreate(Action):
             GROUP_NAME, VM_NAME, vm_parameters)
         async_vm_creation.wait()
 
-        # Tag the VM
-        print('\nTag Virtual Machine')
-        async_vm_update = compute_client.virtual_machines.create_or_update(
-            GROUP_NAME,
-            VM_NAME,
-            {
-                'location': LOCATION,
-                'tags': {
-                    'who-rocks': 'python',
-                    'where': 'on azure'
-                }
-            }
-        )
-        async_vm_update.wait()
-
         # Attach data disk
-        print('\nAttach Data Disk')
+        print('\nAttach Data Disk To Linux Machine')
         async_vm_update = compute_client.virtual_machines.create_or_update(
             GROUP_NAME,
             VM_NAME,
@@ -140,78 +126,45 @@ class VmCreate(Action):
         )
         async_vm_update.wait()
 
-        # Get one the virtual machine by name
-        # print('\nGet Virtual Machine by Name')
-        # virtual_machine = compute_client.virtual_machines.get(
-        #     GROUP_NAME,
-        #     VM_NAME
-        # )
-
-        # Detach data disk
-        # print('\nDetach Data Disk')
-        # data_disks = virtual_machine.storage_profile.data_disks
-        # data_disks[:] = [disk for disk in data_disks if disk.name != 'mydatadisk1']
-        # async_vm_update = compute_client.virtual_machines.create_or_update(
-        #     GROUP_NAME,
-        #     VM_NAME,
-        #     virtual_machine
-        # )
-        # virtual_machine = async_vm_update.result()
-
-        # Deallocating the VM (resize prepare)
-        # print('\nDeallocating the VM (resize prepare)')
-        # async_vm_deallocate = compute_client.virtual_machines.deallocate(
-        #     GROUP_NAME, VM_NAME)
-        # async_vm_deallocate.wait()
-
-        # Update OS disk size by 10Gb
-        # print('\nUpdate OS disk size')
-        # Server is not returning the OS Disk size (None), possible bug in server
-        # if not virtual_machine.storage_profile.os_disk.disk_size_gb:
-        #     print("\tServer is not returning the OS disk size, possible bug in the server?")
-        #     print("\tAssuming that the OS disk size is 256 GB")
-        #     virtual_machine.storage_profile.os_disk.disk_size_gb = 256
-        #
-        # virtual_machine.storage_profile.os_disk.disk_size_gb += 10
-        # async_vm_update = compute_client.virtual_machines.create_or_update(
-        #     GROUP_NAME,
-        #     VM_NAME,
-        #     virtual_machine
-        # )
-        #virtual_machine = async_vm_update.result()
-
         # Start the VM
-        print('\nStart VM')
+        print('\nStart Linux Virtual Machine')
         async_vm_start = compute_client.virtual_machines.start(GROUP_NAME, VM_NAME)
         async_vm_start.wait()
 
-        # Restart the VM
-        # print('\nRestart VM')
-        # async_vm_restart = compute_client.virtual_machines.restart(
-        #     GROUP_NAME, VM_NAME)
-        # async_vm_restart.wait()
+        # Recycling NIC of previous VM
+        print('\nCreating Windows Virtual Machine')
+        vm_parameters = create_w_vm_parameters(nic.id, VM_REFERENCE['windows'])
+        async_vm_creation = compute_client.virtual_machines.create_or_update(
+            GROUP_NAME, W_VM_NAME, vm_parameters)
+        async_vm_creation.wait()
 
-        # Stop the VM
-        # print('\nStop VM')
-        # async_vm_stop = compute_client.virtual_machines.power_off(
-        #     GROUP_NAME, VM_NAME)
-        # async_vm_stop.wait()
+        # Attach data disk
+        print('\nAttach Data Disk To Windows Machine')
+        async_vm_update = compute_client.virtual_machines.create_or_update(
+            GROUP_NAME,
+            W_VM_NAME,
+            {
+                'location': LOCATION,
+                'storage_profile': {
+                    'data_disks': [{
+                        'name': 'mydatadisk1',
+                        'disk_size_gb': 1,
+                        'lun': 0,
+                        'vhd': {
+                            'uri': "http://{}.blob.core.windows.net/vhds/mydatadisk1.vhd".format(
+                                STORAGE_ACCOUNT_NAME)
+                        },
+                        'create_option': 'Empty'
+                    }]
+                }
+            }
+        )
+        async_vm_update.wait()
 
-        # List VMs in subscription
-        # print('\nList VMs in subscription')
-        # for vm in compute_client.virtual_machines.list_all():
-        #     print("\tVM: {}".format(vm.name))
+        print('\nStart Windows Virtual Machine')
+        async_vm_start = compute_client.virtual_machines.start(GROUP_NAME, W_VM_NAME)
+        async_vm_start.wait()
 
-        # List VM in resource group
-        # print('\nList VMs in resource group')
-        # for vm in compute_client.virtual_machines.list(GROUP_NAME):
-        #     print("\tVM: {}".format(vm.name))
-
-        # Delete VM
-        # print('\nDelete VM')
-        # async_vm_delete = compute_client.virtual_machines.delete(
-        #     GROUP_NAME, VM_NAME)
-        # async_vm_delete.wait()
         return
 
 def create_nic(network_client):
@@ -295,5 +248,42 @@ def create_vm_parameters(nic_id, vm_reference):
         },
     }
 
+def create_w_vm_parameters(nic_id, vm_reference):
+    """Create the VM parameters structure.
+    """
+    return {
+        'location': LOCATION,
+        'os_profile': {
+            'computer_name': W_VM_NAME,
+            'admin_username': USERNAME,
+            'admin_password': PASSWORD
+        },
+        'hardware_profile': {
+            'vm_size': 'Standard_DS1'
+        },
+        'storage_profile': {
+            'image_reference': {
+                'publisher': vm_reference['publisher'],
+                'offer': vm_reference['offer'],
+                'sku': vm_reference['sku'],
+                'version': vm_reference['version']
+            },
+            'os_disk': {
+                'name': OS_DISK_NAME,
+                'caching': 'None',
+                'create_option': 'fromImage',
+                'vhd': {
+                    'uri': 'https://{}.blob.core.windows.net/vhds/{}.vhd'.format(
+                        STORAGE_ACCOUNT_NAME, W_VM_NAME+haikunator.haikunate())
+                }
+            },
+        },
+        'network_profile': {
+            'network_interfaces': [{
+                'id': nic_id,
+            }]
+        },
+    }
+
 if __name__ == '__main__':
-    VmCreate.run(VM_Name)
+    VmCreate.run()
